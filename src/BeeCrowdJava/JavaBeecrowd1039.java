1e2c33ce66438a6e8f4cf08738f4d621d6559832
João-Carlos <joaocarlosdosreia@gmail.com>
2023-12-06 18:21:05 -0300

    Solutions for BeeCrowd activities using java 06/12/2023.

--- src/BeeCrowdJava/JavaBeecrowd1039.java
+++ src/BeeCrowdJava/JavaBeecrowd1039.java
@@ -1,4 +1,37 @@
 package BeeCrowdJava;
 
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class JavaBeecrowd1039 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double n1 = sc.nextDouble();
+        double n2 = sc.nextDouble();
+        double n3 = sc.nextDouble();
+        double n4 = sc.nextDouble();
+        double mediaDoAuluno = (((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / (2 + 3 + 4 + 1));
+
+        System.out.println("Media: " + new DecimalFormat(".#").format(mediaDoAuluno));
+
+        if (mediaDoAuluno > 7.0) {
+            System.out.println("Aluno aprovado.");
+        } else if (mediaDoAuluno < 5.0) {
+            System.out.println("Aluno reprovado.");
+        } else if (mediaDoAuluno >= 5.0 && mediaDoAuluno <= 6.9) {
+            System.out.println("Aluno em exame.");
+            double notaDoExame = sc.nextDouble();
+            System.out.println("Nota do exame: " + notaDoExame);
+            double notaFinal = (notaDoExame + mediaDoAuluno) / 2;
+            String sim = "Aluno aprovado.";
+            String nao = "Aluno reprovado.";
+            String resultado = notaFinal > 5.0 ? sim : nao;
+            System.out.println(resultado);
+            System.out.println("Media final: " + new DecimalFormat(".#").format(notaFinal));
+        }
+    }
 }
