9489ef57c69c0c8d6abdffffb75af0e7970e4016
João-Carlos <joaocarlosdosreia@gmail.com>
2025-01-12 12:27:44 -0300

    Studies beecrowd 12/01/2025

--- src/BeeCrowdJava/Main.java
+++ src/BeeCrowdJava/Main.java
@@ -1,7 +1,32 @@
 package BeeCrowdJava;
+
 import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        for (; ; ) {
+            int m = input.nextInt();
+            int n = input.nextInt();
+            int soma = 0;
+
+            if (m == 0 || n == 0) {
+                break;
+            } else if (m > n) {
+                for (; m >= n; n++) {
+                    System.out.print(n + " ");
+                    soma += n;
+                }
+            } else {
+                for (; n >= m; m++) {
+                    System.out.print(m);
+                    soma += m;
+                }
+            }
+            System.out.println("Sum=" + soma);
+        }
+    }
 }
 
