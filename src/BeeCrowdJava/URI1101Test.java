786d157acd59ba1b70d2984e9d95ae7880b491e5
João-Carlos <joaocarlosdosreia@gmail.com>
2025-01-12 12:50:19 -0300

    Studies beecrowd 12/01/2025

--- src/BeeCrowdJava/URI1101Test.java
+++ src/BeeCrowdJava/URI1101Test.java
@@ -5,30 +5,30 @@ import java.util.Scanner;
 public class URI1101Test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        //tried to solve the problem using arrays and for, didn't finish, should have used a for(;;) to scan the m n.
 
-        int[] m = new int[3];
-        int[] n = new int[3];
-
-        for (int i = 0; i < 3; i++) {
-            m[i] = input.nextInt();
-            n[i] = input.nextInt();
+        for (; ; ) {
+            int[] m = new int[]{input.nextInt()};
+            int[] n = new int[]{input.nextInt()};
             int total = 0;
 
-            if (m[i] == 0 || n[i] == 0 ){
-                break;
-            }
-            else if (m[i] > n[i]) {
-                for (int j = n[i]; j <= m[i]; j++) {
-                    System.out.print(j + " ");
-                    total += j;
+            for (int i = 0; ; i++) {
+
+                if (m[i] == 0 || n[i] == 0) {
+                    break;
+                } else if (m[i] >= n[i]) {
+                    for (int j = n[i]; j <= m[i]; j++) {
+                        System.out.print(j + " ");
+                        total += j;
+                    }
+                } else {
+                    for (int k = m[i]; k <= n[i]; k++) {
+                        System.out.print(k + " ");
+                        total += k;
+                    }
                 }
-            } else {
-                for (int k = m[i]; k <= n[i]; k++) {
-                    System.out.print(k + " ");
-                    total += k;
-                }
+                System.out.println("Sum=" + total);
             }
-            System.out.println("Sum=" + total);
         }
     }
 }
